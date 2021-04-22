09b442833a0208dccc5ba2de3f3ad30b6a0a4a0f
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-04-22 13:21:10 +0300

    fix: Answer field empty

--- soa_backend/models/answer.model.js
+++ soa_backend/models/answer.model.js
@@ -1,5 +1,11 @@
+const { DataTypes } = require('sequelize')
+
 module.exports = (sequelize) => {
   sequelize.define('Answer', {
+    answer: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
   }, {
   })
 }
