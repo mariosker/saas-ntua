8814528267f8099b306655affc781bb59e86feb2
Ioannis Kerasiotis <59957934+ikerasiotis@users.noreply.github.com>
2021-05-16 23:31:27 +0300

    footer, links, 404-page, starting home page

--- askmeanything/src/App.js
+++ askmeanything/src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './components/Home'
+import Page404 from './components/404'
 
 const Login = () => {
   return <p>Login</p>
@@ -20,7 +22,9 @@ function App () {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
+        <Route component={Page404} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   )
 }
--- askmeanything/src/components/Home/index.js
+++ askmeanything/src/components/Home/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Home () {
   return (
-    <main>
-
+    <main className='home'>
+        <h1 className='welcomeMessage'>
+          <span>Welcome To </span>
+          <span className='logo--1'>Ask</span>
+          <span className='logo--2'>Me</span>
+          <span className='logo--3'>Anything</span>
+        </h1>
     </main>
   )
 }
