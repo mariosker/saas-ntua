665e0676672520d68b0c8213788fb5f261d9d5d6
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-05-17 15:18:07 +0300

    fix css not working on linux and add annimation

--- askmeanything/src/components/Home/index.js
+++ askmeanything/src/components/Home/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import HomeCard from './HomeCard'
+import TextLoop from 'react-text-loop'
 
 function Home () {
   return (
     <main className='home'>
+      <div className="questionsAsked">
+  <h1>
+          <TextLoop>
+              <span>How to create a React project?</span>
+              <span>How to jumpstart a microservices project?</span>
+              <span>Why Javascript is so weird?</span>
+              <span>What am I doing?</span>
+              <span>How to change careers?</span>
+          </TextLoop>{''}
+  </h1>
+      </div>
+
         <h1 className='welcomeMessage'>
-          <span>Welcome To </span>
-          <span className='logo--1'>Ask</span>
-          <span className='logo--2'>Me</span>
-          <span className='logo--3'>Anything</span>
         </h1>
         <div className='cards'>
           <HomeCard />
